8a14aa7877c402dddf678c6d5638f24dc938adf5
Masoud <masoud.zeinalabedini@gmail.com>
2023-06-21 15:54:15 +0330

    Initiate project to create APIs

---
+++ functions/index.js
@@ -0,0 +1,16 @@
+const functions = require('firebase-functions');
+
+// http request 1
+exports.randomNumber = functions.https.onRequest((request, response) => {
+  const number = Math.round(Math.random() * 100);
+  console.log(number);
+  response.send(number.toString());
+});
+
+// http request 2
+exports.toTheDojo = functions.https.onRequest((request, response) => {
+  response.redirect('https://www.thenetninja.co.uk');
+});
+
+
+//Get people
